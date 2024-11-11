fe1762abd1d0e4ceceffcae7583c41752a0e5ca2
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-11-11 23:06:14 +0100

    Move imgs to public directory

--- src/components/pdf/components/personal-info/personal-info.jsx
+++ src/components/pdf/components/personal-info/personal-info.jsx
@@ -3,7 +3,7 @@ import './personal-info.css'
 export function PersonalInfo() {
     return(
         <div className='personal-info'>
-            <img className='icon' src="./src/assets/avatar.svg" alt="A React logo" />
+            <img className='icon' src="./src/public/avatar.svg" alt="A React logo" />
             <p>A person info</p>
         </div>
         
